1e93852b8123212d7db021d77400450e6849b52f
hoangvu6262 <hoangvubg6262115@gmail.com>
2021-06-11 12:29:06 +0700

    adv

--- js/firestore/chart.js
+++ js/firestore/chart.js
@@ -1,6 +1,7 @@
 const orderReportRef = firebase.firestore().collection("orders");
 const productReportRef = firebase.firestore().collection("products");
 
+// Order Report
 orderReportRef.onSnapshot((snapshot) => {
   // console.log(snapshot);
   const data = snapshot.docs.map((doc) => ({
@@ -46,8 +47,72 @@ orderReportRef.onSnapshot((snapshot) => {
       cutout: true,
     },
   });
+
+  // thống kê người mua
+
+  let labelsUser = [];
+  data.forEach((item) => {
+    if (!labelsUser.includes(item.nameUser)) {
+      labelsUser.push(item.nameUser);
+    }
+  });
+  console.log(labelsUser);
+  let userCount = data.reduce((acc, item) => {
+    if (typeof acc[item.nameUser] == "undefined") {
+      acc[item.nameUser] = 1;
+    } else {
+      acc[item.nameUser] += 1;
+    }
+
+    return acc;
+  }, {});
+
+  console.log(Object.values(userCount));
+
+  const userOrderID = document
+    .getElementById("userOrderChart")
+    .getContext("2d");
+
+  new Chart(userOrderID, {
+    type: "doughnut",
+    data: {
+      labels: labelsUser,
+      datasets: [
+        {
+          data: Object.values(userCount),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(0, 159, 64, 0.2)",
+            "rgba(54, 0, 235, 0.2)",
+            "rgba(255, 206, 0, 0.2)",
+            "rgba(0, 0, 192, 0.2)",
+            "rgba(0, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(0, 159, 64, 1)",
+            "rgba(54, 0, 235, 1)",
+            "rgba(255, 206, 0, 1)",
+            "rgba(0, 0, 192, 1)",
+            "rgba(0, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+          hoverOffset: 4,
+        },
+      ],
+    },
+  });
 });
 
+// Product Report
 productReportRef.onSnapshot((snapshot) => {
   // console.log(snapshot);
   const dataProduct = snapshot.docs.map((doc) => ({
@@ -55,7 +120,7 @@ productReportRef.onSnapshot((snapshot) => {
     ...doc.data(),
   }));
 
-  console.log(dataProduct);
+  // console.log(dataProduct);
 
   let labels = [];
 
@@ -75,7 +140,7 @@ productReportRef.onSnapshot((snapshot) => {
     return acc;
   }, {});
 
-  console.log(Object.values(a));
+  // console.log(Object.values(a));
 
   const ctx = document.getElementById("productChart").getContext("2d");
   new Chart(ctx, {
@@ -92,6 +157,11 @@ productReportRef.onSnapshot((snapshot) => {
             "rgba(255, 206, 86, 0.2)",
             "rgba(75, 192, 192, 0.2)",
             "rgba(153, 102, 255, 0.2)",
+            "rgba(0, 159, 64, 0.2)",
+            "rgba(54, 0, 235, 0.2)",
+            "rgba(255, 206, 0, 0.2)",
+            "rgba(0, 0, 192, 0.2)",
+            "rgba(0, 102, 255, 0.2)",
             "rgba(255, 159, 64, 0.2)",
           ],
           borderColor: [
