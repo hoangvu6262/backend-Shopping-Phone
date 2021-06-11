d51c7d8bfb96abd242c8226ea4069311c25d015d
hoangvu6262 <hoangvubg6262115@gmail.com>
2021-06-11 11:19:01 +0700

    add product report

--- js/firestore/chart.js
+++ js/firestore/chart.js
@@ -64,5 +64,61 @@ productReportRef.onSnapshot((snapshot) => {
       labels.push(product.category);
     }
   });
-  console.log(labels);
+  // console.log(labels);
+  let a = dataProduct.reduce((acc, product) => {
+    if (typeof acc[product.category] == "undefined") {
+      acc[product.category] = 1;
+    } else {
+      acc[product.category] += 1;
+    }
+
+    return acc;
+  }, {});
+
+  console.log(Object.values(a));
+
+  const ctx = document.getElementById("productChart").getContext("2d");
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "thống kê sản phẩm",
+          data: Object.values(a),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            display: true,
+            ticks: {
+              beginAtZero: true,
+              max: 20,
+              min: 0,
+            },
+          },
+        ],
+      },
+    },
+  });
 });
